aea53017576fa3e0c3313baf7f9f4aab4c1fb5e9
koihnl <koihnl@qq.com>
2024-09-05 14:25:40 +0800

    Update logo.js

--- js/logo.js
+++ js/logo.js
@@ -1,77 +1,71 @@
-
-(function loadDependencies() {
-    var echartsScript = document.createElement('script');
-    echartsScript.src = "/js/echarts.min.js";
-    document.head.appendChild(echartsScript);
-
-    var cryptoScript = document.createElement('script');
-    cryptoScript.src = "https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.1.1/crypto-js.min.js";
-    cryptoScript.onload = function() {
-        echartsScript.onload = createKoinlChart; 
+(function loadECharts() {
+    var script = document.createElement('script');
+    script.src = "/js/echarts.min.js";
+    script.onload = function() {
+        var textToDisplay = getText(); 
+        createKoinlChart(textToDisplay);
     };
-    document.head.appendChild(cryptoScript);
+    document.head.appendChild(script);
 })();
 
-
-function decryptText(ciphertext, key) {
-    var bytes = CryptoJS.AES.decrypt(ciphertext, key);
-    return bytes.toString(CryptoJS.enc.Utf8); 
+function getText() {
+    var encodedText = '5oWI5oWI5Y+z5aOr';
+    return decodeURIComponent(escape(atob(encodedText)));
 }
 
-function createKoinlChart() {
+
+function createKoinlChart(text) {
     var chartkoinl = document.createElement('div');
-    Object.assign(chartkoinl.style, {
-        width: '80px',
-        height: '48px',
-        position: 'fixed',
-        top: '5px',
-        right: '6px'
-    });
+    chartkoinl.style.width = '80px';
+    chartkoinl.style.height = '48px';
+    chartkoinl.style.position = 'fixed';
+    chartkoinl.style.top = '5px';
+    chartkoinl.style.right = '6px';
     document.body.appendChild(chartkoinl);
 
     var koinlChart = echarts.init(chartkoinl);
-
-
-    var secretKey = 'mySecretKey'; 
-    var encryptedText = 'U2FsdGVkX1+kRDw0poC8FDHkgfnoXjxQX4qEFBULP+g=';
-
     var option = {
         graphic: {
-            elements: [{
-                type: 'text',
-                left: 'center',
-                top: 'center',
-                style: {
-
-                    text: decryptText(encryptedText, secretKey),  
-                    fontFamily: 'STSong',
-                    fontSize: 20,
-                    fontWeight: 'lighter',
-                    lineDash: [0, 200],
-                    lineDashOffset: 0,
-                    fill: 'transparent',
-                    stroke: '#b44b22',
-                    lineWidth: 1
-                },
-                keyframeAnimation: {
-                    duration: 5000,
-                    loop: false,
-                    keyframes: [{
-                        percent: 0.7,
-                        style: {
-                            fill: 'transparent',
-                            lineDashOffset: 200,
-                            lineDash: [200, 0]
-                        }
-                    }, {
-                        percent: 0.8,
-                        style: { fill: 'transparent' }
-                    }, {
-                        percent: 1,
-                        style: { fill: '#b44b22' }
-                    }]
-                }
-            }]
+            elements: [
+                {
+                    type: 'text',
+                    left: 'center',
+                    top: 'center',
+                    style: {
+                        text: text,
+                        fontFamily: 'STSong',
+                        fontSize: 20,
+                        fontWeight: 'lighter',
+                        lineDash: [0, 200],
+                        lineDashOffset: 0,
+                        fill: 'transparent',
+                        stroke: '#b44b22',
+                        lineWidth: 1
+                    },
+                    keyframeAnimation: {
+                        duration: 5000,
+                        loop: false,
+                        keyframes: [
+                            {
+                                percent: 0.7,
+                                style: {
+                                    fill: 'transparent',
+                                    lineDashOffset: 200,
+                                    lineDash: [200, 0]
+                                }
+                            }, {
+                                percent: 0.8,
+                                style: {
+                                    fill: 'transparent'
+                                }
+                            }, {
+                                percent: 1,
+                                style: {
+                                    fill: '#b44b22'
+                                }
+                            }]
+                    }
+                }]
         }
     };
     koinlChart.setOption(option);
